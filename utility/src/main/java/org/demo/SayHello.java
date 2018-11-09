6612e5ba32db088984c531f0eb1cd0454b2c7b0d
SailHe <396103442@qq.com>
2018-11-09 19:41:17 +0800

    Add package path and a hello world class

---
+++ utility/src/main/java/org/demo/SayHello.java
@@ -0,0 +1,20 @@
+package org.demo;
+
+/**
+ * Description: 无描述类<p>
+ *
+ * Package: org.demo
+ * author: SailHe
+ * date: 2018/11/9 18:38
+ */
+public class SayHello {
+    /**
+     * Descriptions: hello<p>
+     *
+     * author SailHe
+     * date 2018/11/9 19:34
+     */
+    public String sayHello(String name) {
+        return "Hello " + name + "!";
+    }
+}
