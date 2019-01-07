94ec58d2d789f223b6466eb55434ee8b71e25cec
SailHe <396103442@qq.com>
2019-01-07 18:32:50 +0800

    (utility):  Secure Hash Algorithm

---
+++ utility/src/main/java/org/demo/SecurityTester.java
@@ -0,0 +1,27 @@
+package org.demo;
+
+import practice.util.SecureHashStandard;
+
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Description: 无描述类<p>
+ *
+ * @Package: org.demo
+ * @author: SailHe
+ * @date: 2019/1/7 16:36
+ */
+public class SecurityTester {
+    public static void main(String[] args) {
+        SecureHashStandard s256;
+        SecureHashStandard s512;
+        s256 = new SecureHashStandard(SecureHashStandard.EnumSHAType.SHA_256);
+        s512 = new SecureHashStandard(SecureHashStandard.EnumSHAType.SHA_512);
+        String str = "a";
+        System.out.println(s256.calcHashValue(str));
+        System.out.println(s256.calcHashValue(str).equals(s256.calcHashValue(str)));
+
+        System.out.println(s512.calcHashValue(str));
+        System.out.println(s512.calcHashValue(str).equals(s512.calcHashValue(str)));
+    }
+}
